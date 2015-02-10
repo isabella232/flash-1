096bfd694dd54632b33c04dc81cdb8e993f09c00
Alexander Novikov <anovikov@granpm.com>
2015-02-10 02:10:24 -0800

    add CG methods to as2js proxy

--- unitest/html-template/pubnub-as2js-proxy.js
+++ unitest/html-template/pubnub-as2js-proxy.js
@@ -16,14 +16,28 @@ var config = function () {
             revoke: ['callback', 'error'],
             audit: ['callback', 'error'],
             where_now: ['callback', 'error'],
-            state: ['callback', 'error']
+            state: ['callback', 'error'],
+            channel_group: ['callback', 'error'],
+            channel_group_list_channels: ['callback', 'error'],
+            channel_group_list_groups: ['callback', 'error'],
+            channel_group_list_namespaces: ['callback', 'error'],
+            channel_group_remove_channel: ['callback', 'error'],
+            channel_group_remove_group: ['callback', 'error'],
+            channel_group_remove_namespace: ['callback', 'error'],
+            channel_group_add_channel: ['callback', 'error'],
+            channel_group_cloak: ['callback', 'error']
         },
         methods_with_callback_in_args: [
             'history', 'time', 'publish', 'unsubscribe', 'subscribe', 'here_now', 'grant',
-            'audit', 'revoke', 'time', 'where_now', 'state'
-        ],
+            'audit', 'revoke', 'time', 'where_now', 'state',
+            'channel_group', 'channel_group_list_channels', 'channel_group_list_groups', 'channel_group_list_namespaces',
+            'channel_group_remove_channel', 'channel_group_remove_group', 'channel_group_remove_namespace',
+            'channel_group_add_channel', 'channel_group_cloak'],
         async_methods_to_delegate: ['history', 'replay', 'subscribe', 'publish', 'unsubscribe', 'here_now', 'grant', 'revoke',
-            'audit', 'time', 'where_now', 'state'],
+            'audit', 'time', 'where_now', 'state',
+            'channel_group', 'channel_group_list_channels', 'channel_group_list_groups', 'channel_group_list_namespaces',
+            'channel_group_remove_channel', 'channel_group_remove_group', 'channel_group_remove_namespace',
+            'channel_group_add_channel', 'channel_group_cloak'],
         sync_methods_to_delegate: ['set_uuid', 'get_uuid', 'uuid', 'auth', 'set_cipher_key', 'get_cipher_key', 'raw_encrypt',
             'raw_decrypt', 'set_heartbeat', 'get_heartbeat', 'set_heartbeat_interval', 'get_heartbeat_interval']
     };
